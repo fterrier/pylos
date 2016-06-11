79b096d86e3554f2d59cee9f604d58630dd21222
François Terrier <fterrier@gmail.com>
2016-06-11 11:22:53 +0200

    refactoring tracker

--- src/cljs/ui/pylos/tracker.cljs
+++ src/cljs/ui/pylos/tracker.cljs
@@ -4,43 +4,48 @@
    [om-tools.dom :as dom :include-macros true]
    [devcards.core :as dc :refer-macros [defcard defcard-doc defcard-om-next]]))
 
-
-(defn tracker [balls-remaining current player outcome]
-  (let [counter (get balls-remaining current)
-        circle (dom/ul {:class "tracker-circle-list"}
-                       (repeat counter
-                               (dom/li {:class (str "tracker-circle tracker-circle-" (name current))}
-                                       (dom/figure {:class (str "circle circle-" (name current))}
-                                                   counter))))
-        text   (dom/div {:class "tracker-text"} 
+(defn tracker [balls-remaining color state]
+  (let [circle (dom/ul {:class "tracker-circle-list"}
+                       (repeat balls-remaining
+                               (dom/li {:class (str "tracker-circle tracker-circle-" (name color))}
+                                       (dom/figure {:class (str "circle circle-" (name color))}
+                                                   balls-remaining))))
+        text   (dom/div {:class "tracker-text"}
                         (cond
-                          (= outcome current) "Winner!"
-                          (= player current) "Your turn"
+                          (= state :tracker/winner) "Winner!"
+                          (= state :tracker/your-turn) "Your turn"
                           :else ""))]
-    (dom/div {:class (str "tracker-color tracker-color-" (name current))}
-             (if (= :white current) 
-               [text circle]
-               [circle text]))))
+    (dom/div {:class (str "tracker-color tracker-color-" (name color))}
+             [circle text])))
+
+(defui PlayerInfos
+  static om/IQuery
+  (query [this]
+         '[:tracker/balls-remaining :tracker/color :tracker/state :tracker/players])
+  Object
+  (render [this]
+          (let [{:keys [tracker/balls-remaining tracker/color tracker/players tracker/state]} (om/props this)]
+            (dom/div {:class "tracker"}
+                     (tracker balls-remaining color state)))))
+
+(def player-infos (om/factory PlayerInfos))
 
 (defui GameTracker
   static om/IQuery
   (query [this]
-         '[:tracker/balls-remaining :tracker/player :tracker/outcome])
-  Object
-  (render [this]
-          (let [{:keys [tracker/balls-remaining tracker/player tracker/outcome]} (om/props this)]
-            (dom/div {:class "tracker"}
-                     (tracker balls-remaining :white player outcome)
-                     (tracker balls-remaining :black player outcome)))))
+         '[:tracker/player-infos]))
 
 (def game-tracker (om/factory GameTracker))
 
-(defcard last-white-ball
-  (game-tracker
-   {:tracker/player :white
-    :tracker/balls-remaining {:white 15 :black 15}}) {:inspect-data true})
 
-(defcard last-black-ball
-  (game-tracker
-   {:tracker/player :black
-    :tracker/balls-remaining {:black 2 :white 9}}) {:inspect-data true})
+(defcard all-balls
+  (player-infos
+   {:tracker/color :white
+    :tracker/state :tracker/my-turn ;; my-turn / your-turn / winner / game-over
+    :tracker/balls-remaining 15}) {:inspect-data true})
+
+(defcard no-balls
+  (player-infos
+   {:tracker/color :black
+    :tracker/state :tracker/winner
+    :tracker/balls-remaining 0}) {:inspect-data true})
